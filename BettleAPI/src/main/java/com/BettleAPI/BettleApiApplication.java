2bc25d3ff1506a3550e6ab852165bded563b4e41
Sefer Giray Senel <giray.senel@ug.bilkent.edu.tr>
2022-05-04 21:22:39 +0300

    initial commit
    -API commit

---
+++ BettleAPI/src/main/java/com/BettleAPI/BettleApiApplication.java
@@ -0,0 +1,13 @@
+package com.BettleAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BettleApiApplication.class, args);
+	}
+
+}
---
+++ BettleAPI/src/test/java/com/BettleAPI/BettleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BettleAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BettleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
