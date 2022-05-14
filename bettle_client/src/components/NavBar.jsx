b3f3ddd7a2a77943ead45edb3681060781b4de89
Sefer Giray Senel <giray.senel@ug.bilkent.edu.tr>
2022-05-14 20:56:56 +0300

    Navbar bootstrap

--- bettle_client/src/components/NavBar.jsx
+++ bettle_client/src/components/NavBar.jsx
@@ -3,10 +3,20 @@ import { NavLink } from "react-router-dom";
 
 function NavBar(){
     return(
-        <ul className="navbar-list">
-            <li><NavLink to="/" >Home</NavLink></li>
-            <li><NavLink to="/users" >Users</NavLink></li>
-        </ul>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a href="javascript:void(0);" class="navbar-brand m-2" >Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+                <NavLink to="/" className={(navData)=> navData.isActive ? '' : "nav-links"}><a href="javascript:void(0);" class="nav-item nav-link">Home</a></NavLink>
+                <NavLink to="/users" className={(navData)=> navData.isActive ? '' : "nav-links"}><a href="javascript:void(0);" class="nav-item nav-link">Users</a></NavLink>
+                <a href="javascript:void(0);" class="nav-item nav-link">Pricing</a>
+                <a href="javascript:void(0);" class="nav-item nav-link disabled" tabindex="-1" aria-disabled="true">Disabled</a>
+          </div>
+        </div>
+      </nav>
     );
 }
 
