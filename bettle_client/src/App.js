a5e86a0a7adb783a26dbb985fe6fafbdccead580
Sefer Giray Senel <giray.senel@ug.bilkent.edu.tr>
2022-05-15 20:05:56 +0300

    Login Logout frontend

--- bettle_client/src/App.js
+++ bettle_client/src/App.js
@@ -1,17 +1,33 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import React, {useState} from "react";
 import UserComponent from './components/UserComponent';
 import HomeComponent from './components/HomeComponent';
 import NavBar from './components/NavBar';
 import LoginComponent from './components/LoginComponent';
+
 function App() {
+  const [name, setNameInside] = useState({username: (localStorage.getItem('username') !== null ? localStorage.getItem('username') : "") });
+  
+    const setName = (username) => {
+      setNameInside({username});
+      if(username === "")
+        localStorage.removeItem('username');
+      else
+        localStorage.setItem('username', name);
+    }
+
+    const getName = () => {
+      return name.username;
+    }
+
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar setName={setName} getName={getName}/>
       <Routes>
         <Route path='/home' element={<HomeComponent/>} />
         <Route path='/users' element={<UserComponent/>} />
-        <Route path='/' element={<LoginComponent/>} />
+        <Route path='/' element={<LoginComponent setName={setName} getName={getName} />} />
       </Routes>
     </div>
   );
--- bettle_client/src/components/NavBar.jsx
+++ bettle_client/src/components/NavBar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavBar(){
+function NavBar({setName,getName}){
+    console.log(getName());
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <NavLink to="/" className="nav-links">Bettle</NavLink>
@@ -12,6 +13,7 @@ function NavBar(){
           <div className="navbar-nav">
                 <NavLink to="/home" className={(navData)=> navData.isActive ? "nav-links-active" : "nav-links"}>Home</NavLink>
                 <NavLink to="/users" className={(navData)=> navData.isActive ? "nav-links-active" : "nav-links"}>Users</NavLink>
+                {(getName() !== "") ? <button onClick={() => setName("")}>Logout</button> : ""}
           </div>
         </div>
       </nav>
