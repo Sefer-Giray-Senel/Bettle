bc0eb42a181569e7ec968e81d08f33b422b23dcc
osman-serhat-yilmaz <serhat.yilmaz@ug.bilkent.edu.tr>
2022-05-16 20:55:41 +0300

    BetSlip Save done

--- BettleAPI/src/main/java/com/BettleAPI/controller/BetSlipController.java
+++ BettleAPI/src/main/java/com/BettleAPI/controller/BetSlipController.java
@@ -1,11 +1,9 @@
 package com.BettleAPI.controller;
 
-import com.BettleAPI.entity.Bet;
-import com.BettleAPI.entity.BetSlip;
-import com.BettleAPI.service.BetService;
-import com.BettleAPI.service.BetSlipService;
-import com.BettleAPI.service.BettorHasSlipService;
-import com.BettleAPI.service.DisplayService;
+import com.BettleAPI.entity.*;
+import com.BettleAPI.entity.compositeId.DisplayId;
+import com.BettleAPI.entity.compositeId.HasSlipId;
+import com.BettleAPI.service.*;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,17 +11,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.persistence.NoResultException;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "betslip")
+@RequestMapping(path = "bet-slip")
 @RequiredArgsConstructor(onConstructor = @__({@Autowired,@NonNull}))
 public class BetSlipController {
     private final BetSlipService betSlipService;
     private final BetService betService;
     private final DisplayService displayService;
+    private final EditorService editorService;
+    private final BettorService bettorService;
+    private final EditorHasSlipService editorHasSlipService;
+    private final BettorHasSlipService bettorHasSlipService;
 
     @GetMapping("/show")
     public BetSlip getBetSlipById(@RequestParam("bet_slip_id") int id) {
@@ -36,8 +39,36 @@ public class BetSlipController {
     }
 
     @PutMapping
-    public void updateBetSlip(@RequestParam("bet_slip_id") int id, @RequestParam("odd") double odd) { //may need to require calculating the new odd
-        betSlipService.updateBetSlip(id,odd);
+    public void updateBetSlip(@RequestParam("bets") List<Bet> betList, @RequestParam("bet_slip_id") int id) {
+
+        List<Display> displayList = displayService.findAll();
+        for  (Display d: displayList) {
+            if (d.getId().getBetSlipId() == id) {
+                displayService.delete(d.getId());
+            }
+        }   //delete existing displays
+
+        BetSlip betSlip = betSlipService.findOneById(id); //find betslip
+
+        double odd = 1;
+
+        for (Bet k: betList) { //create display objects for the updated betslip
+            odd *= k.getOdd();
+
+            DisplayId displayId = new DisplayId();
+            displayId.setBetSlipId(betSlip.getId());
+            displayId.setBetId(k.getId());
+
+            Display display = new Display();
+            display.setId(displayId);
+            display.setHasOdd(k.getOdd());
+
+            displayService.save(display);
+        }
+
+        //Add has slip logic here too
+
+        betSlipService.updateBetSlip(id, odd);
     }
 
     @GetMapping
@@ -50,18 +81,60 @@ public class BetSlipController {
         return betList; //not sure
     }
 
-/*
+
     @PostMapping
-    public void saveBetSlip(@RequestParam("odd") double odd) {      //betler ne olarak gelicek
-        Game game = new Game();
-        game.setDate(date);
-        game.setFirstTeamName(firstTeamName);
-        game.setSecondTeamName(secondTeamName);
+    public void saveBetSlip(@RequestParam("bets") List<Integer> betIdList, @RequestParam("user_id") int userId) { //is it List<Bet> or List<Integer>
+        BetSlip betSlip = new BetSlip();
+
+        List<Bet> betList = new ArrayList<>();
+        for(int k: betIdList)
+            betList.add(betService.findOneById(k));
 
         // add constraints if exists
 
-        gameService.save(game);
-        throw new ResponseStatusException(HttpStatus.OK, "Game successfully saved");
+        double odd = 1;
+
+        for (Bet k: betList) {
+            odd *= k.getOdd();
+
+            DisplayId displayId = new DisplayId();
+            displayId.setBetSlipId(betSlip.getId());
+            displayId.setBetId(k.getId());
+
+            Display display = new Display();
+            display.setId(displayId);
+            display.setHasOdd(k.getOdd());
+
+            displayService.save(display);
+        }
+
+        betSlip.setOdd(odd);
+        betSlipService.save(betSlip);
+
+        try {
+            Editor editor = editorService.findOneById(userId);
+
+            HasSlipId editorHasSlipId = new HasSlipId();
+            editorHasSlipId.setBetSlipId(betSlip.getId());
+            editorHasSlipId.setUserId(userId);
+
+            EditorHasSlip editorHasSlip = new EditorHasSlip();
+            editorHasSlip.setId(editorHasSlipId);
+
+            editorHasSlipService.save(editorHasSlip);
+        }
+        catch(NoResultException e) {
+            Bettor bettor = bettorService.findOneById(userId);
+
+            HasSlipId bettorHasSlipId = new HasSlipId();
+            bettorHasSlipId.setBetSlipId(betSlip.getId());
+            bettorHasSlipId.setUserId(userId);
+
+            BettorHasSlip bettorHasSlip = new BettorHasSlip();
+            bettorHasSlip.setId(bettorHasSlipId);
+
+            bettorHasSlipService.save(bettorHasSlip);
+        }
+        throw new ResponseStatusException(HttpStatus.OK, "Bet Slip was successfully saved");
     }
-*/
 }
