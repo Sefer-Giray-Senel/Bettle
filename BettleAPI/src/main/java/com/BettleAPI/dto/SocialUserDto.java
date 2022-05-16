b47dd7c744d5d40724597693421ba424cc36711e
osman-serhat-yilmaz <serhat.yilmaz@ug.bilkent.edu.tr>
2022-05-17 01:34:15 +0300

    SocialUserDto done

---
+++ BettleAPI/src/main/java/com/BettleAPI/dto/SocialUserDto.java
@@ -0,0 +1,31 @@
+package com.BettleAPI.dto;
+
+
+import com.BettleAPI.entity.Bet;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class SocialUserDto {
+
+    private String firstName;
+    private String lastName;
+    private String nationality;
+    private String email;
+    private String gender;
+
+    private double balance;
+    private int friendCount;
+
+    private int subscriberCount;
+    private int successfulBetSlipCount;
+
+    private boolean isEditor;
+}
--- BettleAPI/src/main/java/com/BettleAPI/service/UserService.java
+++ BettleAPI/src/main/java/com/BettleAPI/service/UserService.java
@@ -1,20 +1,14 @@
 package com.BettleAPI.service;
 
-import com.BettleAPI.entity.MyUserDetails;
 import com.BettleAPI.entity.User;
 
 import com.BettleAPI.repository.UserRepository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__({@Autowired,@NonNull}))
